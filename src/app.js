c378204b2a0023e8eb417f224973fa26b09e89b7
Nick van der Sangen <nick.vandersangen@gmail.com>
2017-06-12 22:40:20 +0200

    End of 1.3 Your First Express App

--- src/app.js
+++ src/app.js
@@ -1,3 +1,11 @@
 'use strict';
 
 var express = require('express'); // Express is installed, imported and ready to use
+
+var app = express();
+
+app.get("/", (req, res) => {
+	res.send("I Love Treehouse!");
+});
+
+app.listen(3000);
